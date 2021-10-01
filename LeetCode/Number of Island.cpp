57a3e7c4133f4edc35f22735b792380cd2ab6ca7
Restyled.io <commits@restyled.io>
2021-10-01 08:44:42 +0000

    Restyled by astyle

--- LeetCode/Number of Island.cpp
+++ LeetCode/Number of Island.cpp
@@ -1,33 +1,33 @@
 void dfsislands(vector<vector<char>>& grid, int i, int j)
+{
+    if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]!='1')
     {
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]!='1')
-        {
-            return;
-        }
-        
-        grid[i][j]='0';
-        dfsislands(grid,i+1,j);
-        dfsislands(grid,i,j+1);
-        dfsislands(grid,i-1,j);
-        dfsislands(grid,i,j-1);
+        return;
     }
-    
-    int numIslands(vector<vector<char>>& grid) {
-        
-        int n=grid.size(),m=grid[0].size();
-        int count=0;
-        
-        for(int i=0;i<n;i++)
+
+    grid[i][j]='0';
+    dfsislands(grid,i+1,j);
+    dfsislands(grid,i,j+1);
+    dfsislands(grid,i-1,j);
+    dfsislands(grid,i,j-1);
+}
+
+int numIslands(vector<vector<char>>& grid) {
+
+    int n=grid.size(),m=grid[0].size();
+    int count=0;
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
         {
-            for(int j=0;j<m;j++)
+            if(grid[i][j]=='1')
             {
-                if(grid[i][j]=='1')
-                {
-                    
-                    dfsislands(grid,i,j);
-                    count++;
-                }
+
+                dfsislands(grid,i,j);
+                count++;
             }
         }
-        return count;
     }
+    return count;
+}
