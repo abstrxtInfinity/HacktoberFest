e06e05a487e7fe25b5f4fd1607e4f0accf7ce90e
Restyled.io <commits@restyled.io>
2022-10-10 18:49:36 +0000

    Restyled by black

--- LeetCode/minimumDistanceTriangle.py
+++ LeetCode/minimumDistanceTriangle.py
@@ -2,12 +2,12 @@ def minimumTotal(triangle):
     dp = [[x for x in y] for y in triangle]
 
     for i in range(1, len(dp)):
-        dp[i][0] += dp[i-1][0]
+        dp[i][0] += dp[i - 1][0]
         dp[i][len(dp[i]) - 1] += dp[i - 1][len(dp[i]) - 2]
 
     for i in range(2, len(dp)):
         for j in range(1, len(dp[i]) - 1):
-            dp[i][j] += min(dp[i-1][j], dp[i-1][j-1])
+            dp[i][j] += min(dp[i - 1][j], dp[i - 1][j - 1])
 
     return min(dp[-1])
 
