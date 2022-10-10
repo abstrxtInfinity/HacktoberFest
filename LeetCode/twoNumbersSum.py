6a9c52dfe25f2408e425ab986b5d3896adf88824
Restyled.io <commits@restyled.io>
2022-10-10 18:34:56 +0000

    Restyled by autopep8

--- LeetCode/twoNumbersSum.py
+++ LeetCode/twoNumbersSum.py
@@ -1,13 +1,14 @@
 def numbers(arr, target):
     visited = {}
-    visited[arr[0]] = True 
-    
+    visited[arr[0]] = True
+
     for el in range(1, len(arr)):
-        target_el  = target - arr[el]
+        target_el = target - arr[el]
         if visited.get(target_el):
             return True
         visited[arr[el]] = True
-    return False 
+    return False
 
-arr = [3,5,-4,8,11,1,-1,6,4]
-print(numbers(arr, 5))         
+
+arr = [3, 5, -4, 8, 11, 1, -1, 6, 4]
+print(numbers(arr, 5))
