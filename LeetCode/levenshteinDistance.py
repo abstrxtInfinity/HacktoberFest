afb4466d1e77fa138b99ad2d5bfd40b50e6ad889
Restyled.io <commits@restyled.io>
2022-10-10 18:43:06 +0000

    Restyled by autopep8

--- LeetCode/levenshteinDistance.py
+++ LeetCode/levenshteinDistance.py
@@ -4,18 +4,20 @@ def levenshteinDistance(str1, str2):
 
     record = [[x for x in range(len(small) + 1)]] * 2
 
-    for i in range (1 , len(big) + 1):
-        current = [0 for _ in  range(len(small) + 1)]
+    for i in range(1, len(big) + 1):
+        current = [0 for _ in range(len(small) + 1)]
         current[0] = record[0][0] + 1
-        record[1] =  current
+        record[1] = current
 
-        for j in range(1 , len(small) + 1):
+        for j in range(1, len(small) + 1):
             if big[i-1] == small[j-1]:
-                record[1][j] = record[0][j -1]
+                record[1][j] = record[0][j - 1]
             else:
-                record[1][j] = 1 + min(record[1][j-1], record[0][j], record[0][j -1])
-        
+                record[1][j] = 1 + \
+                    min(record[1][j-1], record[0][j], record[0][j - 1])
+
         record[0] = record[1]
     return record[-1][-1]
 
-print(levenshteinDistance('ab','abc'))
+
+print(levenshteinDistance('ab', 'abc'))
